35fa509818cd3043458f63b08a9332d256219d33
Straj <72680335>
2018-10-03 17:45:38 +0300

    Working on armory

--- app/src/main/java/com/npgames/insight/ui/book/armory/EquipmentDialogFragment.java
+++ app/src/main/java/com/npgames/insight/ui/book/armory/EquipmentDialogFragment.java
@@ -1,11 +1,17 @@
 package com.npgames.insight.ui.book.armory;
 
+import android.app.ActionBar;
 import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.npgames.insight.R;
@@ -13,36 +19,51 @@ import com.npgames.insight.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static com.npgames.insight.ui.book.bottom_new.actions.BottomActionConfirmDialog.CONFIRMATION_TEXT;
+
 public class EquipmentDialogFragment extends DialogFragment implements View.OnClickListener{
     public static final String TAG = "EQUIPMENT_DIALOG_TAG";
     public static final String NAME = "NAME";
     public static final String DESCRIPTION = "DESCRIPTION";
-
-    @BindView(R.id.text_view_dialog_fragment_equipment_more_name)
-    protected TextView nameTextView;
-    @BindView(R.id.text_view_dialog_fragment_equipment_more_description)
+    @BindView(R.id.equipment_more_description_text_view)
     protected TextView descriptionTextView;
-    @BindView(R.id.button_dialog_fragment_equipment_more_close)
-    protected Button closeButton;
+    @BindView(R.id.equipment_more_item_picture_image_view)
+    protected ImageView itemPictureImageView;
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+    }
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View dialogView = inflater.inflate(R.layout.dialog_fragment_equipment_more, null);
         ButterKnife.bind(this, dialogView);
 
-        nameTextView.setText(getArguments().getString(NAME));
         descriptionTextView.setText(getArguments().getString(DESCRIPTION));
-        closeButton.setOnClickListener(this);
+        itemPictureImageView.setImageDrawable(getResources().getDrawable(R.drawable.blaster3));
         return dialogView;
     }
 
     @Override
+    public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final String bodyText = getArguments().getString(CONFIRMATION_TEXT);
+
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        getDialog().setOnKeyListener((dialogInterface, i, keyEvent) -> {
+/*            if (i == KeyEvent.KEYCODE_BACK) {
+                deathDialogListener.onClose();
+            }*/
+
+            return true;
+        });
+    }
+
+    @Override
     public void onClick(final View v) {
-        switch(v.getId()) {
-            case R.id.button_dialog_fragment_equipment_more_close:
-                dismiss();
-                break;
-        }
     }
 }
